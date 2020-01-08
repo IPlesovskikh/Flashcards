beedd2f520643d325c667a5a185a9f8c80d8cfab
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2020-01-08 21:25:56 +0300

    added switch for menu

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -7,8 +7,95 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> cards = new LinkedHashMap<>();
+        Mistakes counter = new Mistakes();
+        Logging log = new Logging();
+        String com = null;
+        if (args.length != 0) {
+            try {
+                if ("-import".equals(args[0])) {
+                    importCards(cards, sc, log, counter, args[1]);
+                } else if (args.length > 2 && "-import".equals(args[2])) {
+                    importCards(cards, sc, log, counter, args[3]);
+                }
+            } catch (IOException e) {
+                System.out.println("not found");
+            }
+        }
+        while (true) {
+            System.out.println("Input the action (add, remove, import, export, ask, exit, log, hardtest card, reset stats):");
+            log.saveData("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
+            com = sc.nextLine();
+            log.saveData(com);
+            switch (com) {
+                case "add":
+                    addCards(cards, sc, log);
+                    break;
+                case "remove":
+                    removeCard(cards, sc, log, counter);
+                    break;
+                case "import":
+                    try {
+                        importCards(cards, sc, log, counter, null);
+                    } catch (IOException e) {
+                        System.out.println("not found");
+                    }
+                    break;
+                case "export":
+                    try {
+                        exportCards(cards, sc, log, counter, null);
+                    } catch (IOException e) {
+                        System.out.println("not found");
+                    }
+                    break;
+                case "ask":
+                    askCards(cards, sc, log, counter);
+                    break;
+                case "exit":
+                    if (args.length != 0) {
+                        try {
+                            System.out.println("Bye bye!");
+                            if ("-export".equals(args[0])) {
+                                exportCards(cards, sc, log, counter, args[1]);
+                            } else if (args.length > 2 && "-export".equals(args[2])) {
+                                exportCards(cards, sc, log, counter, args[3]);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("not found");
+                        }
+                        return ;
+                    }
+                    System.out.println("Bye bye!");
+                    return;
+                case "log":
+                    try {
+                        exportLog(log, sc);
+                    } catch (IOException e) {
+                        System.out.println("not found");
+                        log.saveData("not found");
+                    }
+                    break;
+                case "hardest card":
+                    findHardTest(log, counter);
+                    break;
+                case "reset stats":
+                    counter.reset();
+                    System.out.println("Card statistics has been reset.");
+                    log.saveData("Card statistics has been reset.");
+                    break;
+                default:
+                    System.out.println("Commande \"" + com + "\" doesn't exist");
+                    log.saveData("Commande \"" + com + "\" doesn't exist");
+                    break;
+            }
+        }
+    }
+
     private static int checkCard(String card, Map<String, String> cards) {
-        for (String check : cards.keySet()){
+        for (String check : cards.keySet()) {
             if (card.equals(check)) {
                 return 1;
             }
@@ -17,7 +104,7 @@ public class Main {
     }
 
     private static int checkDefinition(String definition, Map<String, String> cards) {
-        for (String check : cards.values()){
+        for (String check : cards.values()) {
             if (definition.equals(check)) {
                 return 1;
             }
@@ -32,19 +119,19 @@ public class Main {
         log.saveData("The card:");
         tempCard = sc.nextLine();
         log.saveData(tempCard);
-        if(checkCard(tempCard, cards) == 1) {
+        if (checkCard(tempCard, cards) == 1) {
             System.out.println("The card \"" + tempCard + "\" already exists.");
             log.saveData("The card \"" + tempCard + "\" already exists.");
-            return ;
+            return;
         }
         System.out.println("The definition of the card:");
         log.saveData("The definition of the card:");
         tempDefinition = sc.nextLine();
         log.saveData(tempDefinition);
-        if(checkDefinition(tempDefinition, cards) == 1) {
+        if (checkDefinition(tempDefinition, cards) == 1) {
             System.out.println("The definition \"" + tempDefinition + "\" already exists.");
             log.saveData("The definition \"" + tempDefinition + "\" already exists.");
-            return ;
+            return;
         }
         cards.put(tempCard, tempDefinition);
         System.out.println("The pair (" + tempCard + ":" + tempDefinition + ") has been added.");
@@ -52,9 +139,10 @@ public class Main {
     }
 
     private static int checkAnswer(String answer, String response, Map<String, String> cards, Logging log) {
-        for(var entry : cards.entrySet()) {
+        for (var entry : cards.entrySet()) {
             if (entry.getValue().equals(response)) {
-                System.out.println("Wrong answer. The correct one is \"" + answer + "\", you've just written the definition of \"" + entry.getKey() + "\".");;
+                System.out.println("Wrong answer. The correct one is \"" + answer + "\", you've just written the definition of \"" + entry.getKey() + "\".");
+                ;
                 log.saveData("Wrong answer. The correct one is \"" + answer + "\", you've just written the definition of \"" + entry.getKey() + "\".");
                 return 1;
             }
@@ -67,7 +155,7 @@ public class Main {
         log.saveData("Card:");
         String key = sc.nextLine();
         log.saveData(key);
-        if(cards.containsKey(key)) {
+        if (cards.containsKey(key)) {
             cards.remove(key);
             counter.deleteCard(key);
             System.out.println("The card has been removed.");
@@ -85,7 +173,7 @@ public class Main {
         try {
             count = sc.nextInt();
             log.saveData(String.valueOf(count));
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("You must print number" + e.getMessage());
             log.saveData("You must print number" + e.getMessage());
         }
@@ -114,8 +202,8 @@ public class Main {
         }
     }
 
-    private static void exportCards(Map<String, String> cards,  Scanner sc, Logging log, Mistakes counter, String path) throws IOException {
-        if(path == null) {
+    private static void exportCards(Map<String, String> cards, Scanner sc, Logging log, Mistakes counter, String path) throws IOException {
+        if (path == null) {
             System.out.println("File name:");
             log.saveData("File name:");
             path = sc.nextLine();
@@ -134,7 +222,7 @@ public class Main {
     }
 
     private static void importCards(Map<String, String> cards, Scanner sc, Logging log, Mistakes counter, String path) throws IOException {
-        if(path == null) {
+        if (path == null) {
             System.out.println("File name:");
             log.saveData("File name:");
             path = sc.nextLine();
@@ -145,11 +233,11 @@ public class Main {
         String tempValue = null;
         int tempMistakes = 0;
         int sum = 0;
-        for(int i = 0; i < list.size(); i = i + 3) {
+        for (int i = 0; i < list.size(); i = i + 3) {
             tempKey = list.get(i);
             tempValue = list.get(i + 1);
             tempMistakes = Integer.valueOf(list.get(i + 2));
-            if(cards.putIfAbsent(tempKey, tempValue) != null) {
+            if (cards.putIfAbsent(tempKey, tempValue) != null) {
                 cards.remove(tempKey);
                 cards.put(tempKey, tempValue);
             }
@@ -177,98 +265,19 @@ public class Main {
         if (keys == null) {
             System.out.println("There are no cards with errors.");
             log.saveData("There are no cards with errors.");
-        } else if (keys.size() == 1){
+        } else if (keys.size() == 1) {
             System.out.println("The hardest card is \"" + keys.get(0) + "\". You have " + counter.getNumberOfMistakes(keys.get(0)) + " errors answering it.");
             log.saveData("The hardest card is \"" + keys.get(0) + "\". You have " + counter.getNumberOfMistakes(keys.get(0)) + " errors answering it.");
         } else {
             System.out.print("The hardest cards are ");
             log.saveData("The hardest cards are ");
             int i = 0;
-            while(i < keys.size())
-            {
+            while (i < keys.size()) {
                 System.out.print("\"" + keys.get(i) + "\"");
                 log.saveData("\"" + keys.get(i) + "\"");
                 i++;
             }
             System.out.println(". You have " + counter.getNumberOfMistakes(keys.get(0)) + " errors answering them.");
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Map<String, String> cards = new LinkedHashMap<>();
-        Mistakes counter = new Mistakes();
-        Logging log = new Logging();
-        String com = null;
-        if(args.length != 0) {
-            try {
-                if("-import".equals(args[0])) {
-                    importCards(cards, sc, log, counter, args[1]);
-                } else if(args.length > 2 && "-import".equals(args[2])) {
-                    importCards(cards, sc, log, counter, args[3]);
-                }
-
-            } catch(IOException e) {
-                System.out.println("not found");
-            }
-        }
-        while (true) {
-            System.out.println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
-            log.saveData("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
-            com = sc.nextLine();
-            log.saveData(com);
-            if ("add".equals(com)) {
-                addCards(cards, sc, log);
-            } else if("remove".equals(com)) {
-                removeCard(cards, sc, log, counter);
-            } else if("import".equals(com)) {
-                try {
-                    importCards(cards, sc, log, counter, null);
-                } catch(IOException e) {
-                    System.out.println("not found");
-                }
-            } else if("export".equals(com)) {
-                try {
-                    exportCards(cards, sc, log, counter, null);
-                } catch(IOException e) {
-                    System.out.println("not found");
-                }
-            } else if("ask".equals(com)) {
-                askCards(cards, sc, log, counter);
-            } else if("exit".equals(com)) {
-                if(args.length != 0) {
-                    try {
-                        System.out.println("Bye bye!");
-                        if("-export".equals(args[0])) {
-                            exportCards(cards, sc, log, counter, args[1]);
-                        } else if(args.length > 2 && "-export".equals(args[2])){
-                            exportCards(cards, sc, log, counter, args[3]);
-                        }
-                    } catch(IOException e) {
-                        System.out.println("not found");
-                    }
-                    break ;
-                }
-                System.out.println("Bye bye!");
-                break ;
-            } else if("log".equals(com)) {
-                try {
-                    exportLog(log, sc);
-                } catch(IOException e) {
-                    System.out.println("not found");
-                    log.saveData("not found");
-                }
-            }  else if("hardest card".equals(com)) {
-                findHardTest(log, counter);
-            }  else if("reset stats".equals(com)) {
-                counter.reset();
-                System.out.println("Card statistics has been reset.");
-                log.saveData("Card statistics has been reset.");
-            }
-            else {
-                System.out.println("Commande \"" + com + "\" doesn't exist");
-                log.saveData("Commande \"" + com + "\" doesn't exist");
-            }
         }
     }
 }
